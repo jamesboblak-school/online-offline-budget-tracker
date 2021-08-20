26a74f3b4043190b6b7b1e9e7cdb50c23fa97bed
jamesboblak <james@skepticalrecords.com>
2021-08-20 16:43:38 -0700

    mongo

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 // =============================
 // ADD || "mogodb+srv:// . . . "
 // =============================
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
